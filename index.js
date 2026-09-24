1542c9ff7acecd641863ff3eb6997fbed824a124
tomcung10-eng <tomcung10@gmail.com>
2026-09-25 00:37:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ app.get('/com.dts.freefireth/pack', (req,res)=>{
     package: "com.dts.freefireth",
     game_name: "FREE FIRE",
     Grafix: { hd: true, ultra: true },
-    AimHead: { enabled: true, value: "80%", aim: 80 }
+    AimLockHead: { enabled: true, value: "80%", aimlock: 80 }
   });
 });
 
 app.get('/com.dts.freefireth/aimhead60', (req,res)=>{
   res.json({
     package: "com.dts.freefireth",
-    AimHead80: { enabled: true, value: "80%", aim: 80 }
+    AimLockHead: { enabled: true, value: "80%", aimlock: 80 }
   });
 });
 
